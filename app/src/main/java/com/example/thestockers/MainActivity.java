fea12d2d2e11a435239ae2d9703515c4c1a6cd0b
Yun Chi Leong <yleong@uwyo.edu>
2021-11-01 16:15:20 -0600

    Navigation bar with switching fragments

--- app/src/main/java/com/example/thestockers/MainActivity.java
+++ app/src/main/java/com/example/thestockers/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.thestockers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+        Set<Integer> topLevelDestinations = new HashSet<>();
+        topLevelDestinations.add(R.id.shoppingListFragment);
+        topLevelDestinations.add(R.id.scanFragment);
+        topLevelDestinations.add(R.id.homeFragment);
+        topLevelDestinations.add(R.id.summaryFragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(topLevelDestinations).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
+
 }
