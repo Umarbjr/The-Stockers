a2618aa290c7436706292dfad1f483631fd1382c
Yunchi Leong <yunchileong@gmail.com>
2022-01-26 21:52:35 -0700

    jan26 9:52pm commit

--- app/src/main/java/com/example/thestockers/ScannerActivity.java
+++ app/src/main/java/com/example/thestockers/ScannerActivity.java
@@ -1,10 +1,13 @@
 package com.example.thestockers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ScannerActivity extends AppCompatActivity {
 
