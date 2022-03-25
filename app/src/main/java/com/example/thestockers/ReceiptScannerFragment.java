9b4b1c51ea2b2975e6ac768c4cc87c649d8ebf7f
Yunchi Leong <yunchileong@gmail.com>
2022-03-25 04:55:40 -0600

    Basic text recognition working

--- app/src/main/java/com/example/thestockers/ReceiptScannerFragment.java
+++ app/src/main/java/com/example/thestockers/ReceiptScannerFragment.java
@@ -39,12 +39,20 @@ import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.TextRecognizerOptions;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ReceiptScannerFragment extends Fragment {
     private ImageView capturedImage;
     private Button captureBtn, addAllBtn;
     private TextView resultTV;
     private Bitmap imageBitmap;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    int CAMERA_PERMISSION_CODE = 200;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,11 +67,8 @@ public class ReceiptScannerFragment extends Fragment {
             public void onClick(View v) {
                if(checkPermissions()) {
                    captureImage();
-                   detectText();
                }else{
-                   //requestPermission();
-                   Toast.makeText(getActivity(), "This feature is unavailable because it " +
-                           "requires a permission that the user has denied", Toast.LENGTH_LONG).show();
+                   requestPermission();
                }
             }
         });
@@ -77,82 +82,96 @@ public class ReceiptScannerFragment extends Fragment {
         return view;
     }
 
+    // returns true when camera permission is granted.
     private boolean checkPermissions(){
-        int cameraPermission = ContextCompat.checkSelfPermission(getActivity(), CAMERA);
+        int cameraPermission = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA);
         return cameraPermission == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void requestPermission(){
-        int PERMISSION_CODE = 200;
-        //ActivityCompat.requestPermissions(getActivity(), new String[]{CAMERA}, PERMISSION_CODE);
-        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{CAMERA}, CAMERA_PERMISSION_CODE);
     }
 
-    private void captureImage(){
+
+    public void captureImage(){
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePicture.resolveActivity(getActivity().getPackageManager())!=null){
-           mStartForResult.launch(takePicture);
-        }
+        mStartForResult.launch(takePicture);
     }
 
     // Register the permissions callback, which handles the user's response to the
     // system permissions dialog. Save the return value, an instance of
-    // ActivityResultLauncher, as an instance variable.
-    private ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    Toast.makeText(getActivity(), "Permission Granted.", Toast.LENGTH_SHORT).show();
-                    captureImage();
-                } else {
-                    Toast.makeText(getActivity(), "This feature is unavailable because it " +
-                            "requires a permission that the user has denied", Toast.LENGTH_LONG).show();
-                }
-            });
-
-    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+    // ActivityResultLauncher, as an instance variable
+   ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == REQUEST_IMAGE_CAPTURE
-                            && result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getResultCode() == REQUEST_IMAGE_CAPTURE || result.getResultCode()==Activity.RESULT_OK) {
                         Intent intent = result.getData();
                         Bundle extras = intent.getExtras();
                         imageBitmap = (Bitmap) extras.get("data");
                         capturedImage.setImageBitmap(imageBitmap);
+                        capturedImage.setRotation(90);
+                        detectText();
                     }
                 }
-
             });
 
+
     private void detectText(){
-        InputImage image = InputImage.fromBitmap(imageBitmap, 0);
+        InputImage image = InputImage.fromBitmap(imageBitmap, 90);
         TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-        Task<Text> result = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+        Task<Text> result = recognizer.process(image).addOnSuccessListener(new OnSuccessListener<Text>(){
             @Override
-            public void onSuccess(@NonNull Text text) {
-                StringBuilder result = new StringBuilder();
-                for(Text.TextBlock block: text.getTextBlocks()){
-                    String blockText = block.getText();
-                    Point[] blockCornerPoint = block.getCornerPoints();
-                    Rect blockFrame = block.getBoundingBox();
-                    for(Text.Line line: block.getLines()){
-                        String lineText = line.getText();
-                        Point[] lineCornerPoint = line.getCornerPoints();
-                        Rect lineRect = line.getBoundingBox();
-                        for (Text.Element element: line.getElements()){
-                            String elementText = element.getText();
-                            result.append(elementText);
-                        }
-                        resultTV.setText(blockText);
+            public void onSuccess(@NonNull Text visionText) {
+                processImage(visionText);
+            }
+        }).addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(), "Failed to detect text from image.",
+                                               Toast.LENGTH_SHORT).show();
                     }
+                });
+    }
+
+    private void processImage(Text text){
+        StringBuilder result = new StringBuilder();
+        for (Text.TextBlock block : text.getTextBlocks()) {
+            String blockText = block.getText();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (Text.Line line : block.getLines()) {
+                String lineText = line.getText();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (Text.Element element : line.getElements()) {
+                    String elementText = element.getText();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                    result.append(elementText);
                 }
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), "Failed to detect text from image."+e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
+            resultTV.setText(blockText);
+        }
 
     }
+
+    /*public static File getReceiptFile() throws IOException {
+        File receipt_file = BitmapToJPEG(imageBitmap);
+        return receipt_file;
+    }
+
+    private File BitmapToJPEG(Bitmap bitmap) throws IOException {
+        File receiptFile = new File(getActivity().getCacheDir(), "receipt-image.jpeg");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+        byte[] bitmapData = out.toByteArray();
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(bitmapData);
+        fos.flush();
+        fos.close();
+        return receiptFile;
+    }*/
+
 }
